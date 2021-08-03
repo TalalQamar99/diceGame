7f6750d2e86829b55bbf8bf10ebeecbcef7bcaf3
TalalQamar99 <61664000+TalalQamar99@users.noreply.github.com>
2021-08-03 15:40:48 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ var play2 = Math.floor((Math.random())*6 + 1);
 var img_all = document.querySelectorAll("img");
 var img1 = img_all[0];
 var img2 = img_all[1];
-var upd_play1 = "/images/dice"+play1+".png";
-var upd_play2 = "/images/dice"+play2+".png";
+var upd_play1 = "images/dice"+play1+".png";
+var upd_play2 = "images/dice"+play2+".png";
 img1.setAttribute("src",upd_play1);
 img2.setAttribute("src",upd_play2);
 if (play1 > play2)
